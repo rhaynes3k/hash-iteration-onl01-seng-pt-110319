0e5298c239c6e691bfbade5d81dbdac126f1d60b
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-19 06:56:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,12 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
 end
 
-
+def happy_birthday(birthday_kids)
+  birthday_kids.sort_by { |name, age| age }
+    birthday_kids.each do|k, a|
+      if a <= 12
+        puts "Happy Birthday #{k}! You are now #{a} years old!"
+      end
+    end
+end
+end
