470b67bcc9ffe45f299a25043cde187e33f1cb28
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-19 07:02:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do|k, a|
     if a <= 12
       puts "Happy Birthday #{k}! You are now #{a} years old!"
+    else
+      puts "You are too old for this."
     end
   end
 end
