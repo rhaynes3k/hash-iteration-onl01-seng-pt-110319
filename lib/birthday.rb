6b7779a5844fd3e42baed7712bc16b4c8aebcad5
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-19 06:29:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each{|k, a|puts "Happy Birthday #{k}! You are now #{a} years old!"}
+  birthday_kids.each{|k, a|if a <= 12 puts "Happy Birthday #{k}! You are now #{a} years old!"}
   # add your code snippet here!
 end
 
